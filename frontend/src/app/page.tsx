4254c8b27c627e8bb7bb41dde6ab9806d0a8e4bf
MartinBallay <ballay.martin307@gmail.com>
2025-10-14 03:07:10 +0200

    test(pipelines): add new line

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-3xl p-8">
       <h2 className="text-3xl font-bold text-cyan-600">Tailwind OK</h2>
+      <h2 className="text-3xl font-bold text-cyan-600">Tailwind OK</h2>
+      <h2 className="text-3xl font-bold text-cyan-600">Tailwind OK</h2>
     </main>
   );
 }
