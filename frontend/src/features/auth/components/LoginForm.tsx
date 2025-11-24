0cff963e4e6f7b1b77914818de054292a3478006
Kristian Slatkovský <slatkovskykristian@gmail.com>
2025-11-24 19:31:34 +0100

    oprava výpisu v notifikácii pri logine

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -76,10 +76,10 @@ export default function LoginForm() {
       }
     } catch (err: any) {
       const message =
+        err.response?.data?.message ||
         err.response?.data?.error_description ||
         err.response?.data?.error ||
         err.response?.data?.detail ||
-        err.response?.data?.message ||
         msgs.auth.errorMsg;
       console.error(msgs.auth.errorTitle, err.response?.data || err.message);
       notifyWarning({
