ebcd970c6dd5c3d9f036b11525e0063803cce298
Kristian Slatkovský <slatkovskykristian@gmail.com>
2026-01-11 13:45:40 +0100

    update nahladov rozhrania

--- frontend/src/shared/data/roleSectionDatas.ts
+++ frontend/src/shared/data/roleSectionDatas.ts
@@ -1,7 +1,7 @@
 import { RoleType } from "@shared-types/core/common";
 
 export const rolePreviewImages: Record<RoleType, string> = {
-  student: "/images/student_dash.jpg",
-  company: "/images/firma_dash.jpg",
-  garant: "/images/garant_dash.jpg",
+  student: "/images/student_dashboard.png",
+  company: "/images/firma_dashboard.png",
+  garant: "/images/garant_dashboard.png",
 };
